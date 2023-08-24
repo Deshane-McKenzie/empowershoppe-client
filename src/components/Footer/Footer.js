442495bafe52b3093cffac09fad4bd9d7b55108c
Deshane McKenzie <122234024+Deshane-McKenzie@users.noreply.github.com>
2023-08-24 01:51:08 -0400

    saved footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,12 @@ import './Footer.scss';
 
 function Footer() {
     return (
+      <>
       <div>
         <h1>Hello, React!</h1>
         <p>This is a basic React functional component.</p>
       </div>
+      </>
     );
   };
   
