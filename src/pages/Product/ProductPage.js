6ddf85ce561171d20d27014763a4e7a3624b7a84
Deshane McKenzie <122234024+Deshane-McKenzie@users.noreply.github.com>
2023-08-25 15:35:59 -0400

    completed the product list and designed it for mobile/tablet/desktop screen sizes on the product page. Also connected the backend to the front end using axios calls

--- src/pages/Product/ProductPage.js
+++ src/pages/Product/ProductPage.js
@@ -1,12 +1,42 @@
-
-
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import ProductPageList from '../../components/ProductPageList/ProductPageList';
+import './ProductPage.scss';
 
 function ProductPage() {
+
+  const [productDetails, setProductDetails] = useState([]);
+  const [activeProduct, setActiveProduct] = useState({});
+
+  const {product_id} = useParams();
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:8000/product')
+    .then((response) => {
+      setProductDetails(response.data);
+
+      const productId = product_id ?? response.data[0].product_id
+
+     return axios.get(`http://localhost:8000/product/${productId}`)
+
+    })
+    .then((response) => {
+      setActiveProduct(response.data)
+      console.log(response.data)
+    })
+    .catch((err) => { console.error(err) })
+  }, [product_id]);
+
+
     return (
+      <>
       <div>
-        <h1>Hello, React!</h1>
-        <p>This is a basic React functional component.</p>
+        <img src={activeProduct[0].product_image} className="product-img" alt="Product" />
+        <ProductPageList productDetails={productDetails}  activeProduct={activeProduct} />
       </div>
+      </>
     );
   };
   
