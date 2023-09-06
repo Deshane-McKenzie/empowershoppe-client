ba8466c457749988d8a2757ddc5a6b9f747de880
Deshane McKenzie <122234024+Deshane-McKenzie@users.noreply.github.com>
2023-09-06 16:04:16 -0400

    completed design and styling for hero on mobile/tablet/desktop screen sizes. made sure shop now button routes correctly

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,4 +1,5 @@
 import './HomePage.scss';
+import { useNavigate } from 'react-router';
 import Hero from '../../assets/images/Hero.jpg';
 import ShowcaseDesignImage from '../../assets/images/ShowcaseDesign.jpg';
 import EasyCheckoutImage from '../../assets/images/EasyCheckout.jpg';
@@ -6,10 +7,23 @@ import EasyCheckoutImage from '../../assets/images/EasyCheckout.jpg';
 
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const handleShopNowNavigation = () => {
+    navigate('/product')
+  }
+
     return (
       <>
-      <img src={Hero} className="hero" alt="Hero" />
       <div>
+        <div className="hero">
+          <img src={Hero} className="hero__img" alt="Hero" />
+          <div className="hero--overlay"></div>
+            <div className="hero__title-wrapper">
+              <h1 className="hero__title">Shop Aspiring Entrepreneur Essential Tools</h1>
+              <button className="hero__btn" onClick={handleShopNowNavigation} >Shop Now</button>
+            </div>
+        </div>
         <h2>Featured Items</h2>
         <h3 className="help__title">How We Help <span className="help">You!</span></h3>
         <div className="help__container-showcase">
@@ -20,7 +34,7 @@ function HomePage() {
             </div>
         </div>
         <div className="help__container-checkout">
-        <img src={EasyCheckoutImage} className="help__img--hidden" alt="Easy Checkout" />
+          <img src={EasyCheckoutImage} className="help__img--hidden" alt="Easy Checkout" />
             <div className="help__checkout-wrapper">
               <h4 className="help__heading-checkout">2. Effortless Checkout Experience:</h4>
               <p className="help__description-checkout">At EmpowerShoppe, we understand that time is of the essence for aspiring entrepreneurs. Our user-friendly checkout process ensures a seamless and hassle-free transaction, allowing entrepreneurs to swiftly secure the tools they need to drive their business forward. With just a few clicks, you can complete your purchase and get back to focusing on what truly matters – building and growing your brand. Streamlined and intuitive, our easy checkout process saves you valuable time and ensures your entrepreneurial journey remains uninterrupted.</p>
