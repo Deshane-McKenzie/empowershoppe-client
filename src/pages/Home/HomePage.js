d4a8dc8e4f855aec9be9bd308d1837dcb94d1e08
Deshane McKenzie <122234024+Deshane-McKenzie@users.noreply.github.com>
2023-09-06 12:09:10 -0400

    completed the how we help you section of the home page for mobile/tablet/desktop screen sizes. Also added the hero image

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,4 +1,5 @@
 import './HomePage.scss';
+import Hero from '../../assets/images/Hero.jpg';
 import ShowcaseDesignImage from '../../assets/images/ShowcaseDesign.jpg';
 import EasyCheckoutImage from '../../assets/images/EasyCheckout.jpg';
 
@@ -7,22 +8,24 @@ import EasyCheckoutImage from '../../assets/images/EasyCheckout.jpg';
 function HomePage() {
     return (
       <>
+      <img src={Hero} className="hero" alt="Hero" />
       <div>
         <h2>Featured Items</h2>
-        <h3>How We Help <span>You!</span></h3>
-        <div>
-          <img src={ShowcaseDesignImage} alt="Showcase Design" />
-            <div>
-              <h4>Aesthetic and Functional Design Showcase:</h4>
-              <p>Our e-commerce website not only sells products but also acts as a living example of effective web design and user experience. Aspiring entrepreneurs can explore this website to gain insights into best practices for e-commerce layout, navigation, and aesthetics, setting a high standard for your own online brands.</p>
+        <h3 className="help__title">How We Help <span className="help">You!</span></h3>
+        <div className="help__container-showcase">
+          <img src={ShowcaseDesignImage} className="help__img" alt="Showcase Design" />
+            <div className="help__showcase-wrapper">
+              <h4 className="help__heading">1. Aesthetic and Functional Design Showcase:</h4>
+              <p className="help__description">Our e-commerce website not only sells products but also acts as a living example of effective web design and user experience. Aspiring entrepreneurs can explore this website to gain insights into best practices for e-commerce layout, navigation, and aesthetics, setting a high standard for your own online brands.</p>
             </div>
         </div>
-        <div>
-            <div>
-              <h4>Effortless Checkout Experience:</h4>
-              <p>At EmpowerShoppe, we understand that time is of the essence for aspiring entrepreneurs. Our user-friendly checkout process ensures a seamless and hassle-free transaction, allowing entrepreneurs to swiftly secure the tools they need to drive their business forward. With just a few clicks, you can complete your purchase and get back to focusing on what truly matters – building and growing your brand. Streamlined and intuitive, our easy checkout process saves you valuable time and ensures your entrepreneurial journey remains uninterrupted.</p>
+        <div className="help__container-checkout">
+        <img src={EasyCheckoutImage} className="help__img--hidden" alt="Easy Checkout" />
+            <div className="help__checkout-wrapper">
+              <h4 className="help__heading-checkout">2. Effortless Checkout Experience:</h4>
+              <p className="help__description-checkout">At EmpowerShoppe, we understand that time is of the essence for aspiring entrepreneurs. Our user-friendly checkout process ensures a seamless and hassle-free transaction, allowing entrepreneurs to swiftly secure the tools they need to drive their business forward. With just a few clicks, you can complete your purchase and get back to focusing on what truly matters – building and growing your brand. Streamlined and intuitive, our easy checkout process saves you valuable time and ensures your entrepreneurial journey remains uninterrupted.</p>
             </div>
-          <img src={EasyCheckoutImage} alt="Easy Checkout" />
+          <img src={EasyCheckoutImage} className="help__img-checkout" alt="Easy Checkout" />
         </div>
       </div>
       </>
